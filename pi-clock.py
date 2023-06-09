9fcfe0ce7437e55497c3ad6fd2874b70fca35e1e
Brian Dauernheim <4228516+BrianDau@users.noreply.github.com>
2023-06-09 17:17:02 -0400

    Cleanup and 12h time

--- pi-clock.py
+++ pi-clock.py
@@ -1,25 +1,25 @@
-# code modified, tweaked and tailored from code by bertwert 
+# code modified, tweaked and tailored from code by bertwert
 # on RPi forum thread topic 91796
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
- 
+
 # GPIO ports for the 7seg pins
 segments =  (11,4,23,8,7,10,18,25)
 # 7seg_segment_pins (11,7,4,2,1,10,5,3) +  100R inline
- 
+
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
- 
-# GPIO ports for the digit 0-3 pins 
+
+# GPIO ports for the digit 0-3 pins
 digits = (22,27,17,24)
 # 7seg_digit_pins (12,9,8,6) digits 0-3 respectively
- 
+
 for digit in digits:
     GPIO.setup(digit, GPIO.OUT)
     GPIO.output(digit, 1)
- 
+
 num = {' ':(0,0,0,0,0,0,0),
     '0':(1,1,1,1,1,1,0),
     '1':(0,1,1,0,0,0,0),
@@ -31,11 +31,17 @@ num = {' ':(0,0,0,0,0,0,0),
     '7':(1,1,1,0,0,0,0),
     '8':(1,1,1,1,1,1,1),
     '9':(1,1,1,1,0,1,1)}
- 
+
 try:
+    currtime="9999"
     while True:
         n = time.ctime()[11:13]+time.ctime()[14:16]
-        s = str(n).rjust(4)
+        ## s = str(n).rjust(4)
+        strt = time.strftime("%-I%M")
+        if len(strt) == 3:
+            s = " " + strt
+        else:
+            s = strt
         for digit in range(4):
             for loop in range(0,7):
                 GPIO.output(segments[loop], num[s[digit]][loop])
